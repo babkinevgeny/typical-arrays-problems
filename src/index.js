95c129c296d53a15033bd3670dadbdbe508dda4e
babkinevgeny <babkinevgeny@gmail.com>
2020-09-30 11:08:10 +0300

    feat: add min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+  const resultOfCheckArr = checkArgs(arr);
+
+  if (resultOfCheckArr === 0 ) {
+    return 0;
+  }
+
+  const min = Math.min(...arr);
+
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+  const resultOfCheckArr = checkArgs(arr);
+
+  if (resultOfCheckArr === 0) {
+    return 0;
+  }
+
+  const max = Math.max(...arr);
+
+  return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  const resultOfCheckArr = checkArgs(arr);
+
+  if (resultOfCheckArr === 0) {
+    return 0;
+  }
+
+  const avg = arr.reduce((acc, current) => acc + current, 0) / arr.length;
+
+  return avg;
 }
+
+const checkArgs = arr => !arr || arr.length === 0 ? 0 : 1;
